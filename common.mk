757ef9fa467010d1b5d21354bf3606a52972157d
tilaksidduram <tilaksidduram@gmail.com>
2017-01-05 00:34:06 +0530

    smdk4412: actually build OSS libraries
    * build the OSS libfimc, hwconverter, hwjpeg

--- common.mk
+++ common.mk
@@ -62,7 +62,10 @@ PRODUCT_PACKAGES := \
     hwcomposer.exynos4 \
     libExynosHWCService \
     libExynosIPService \
+    libfimc \
     libfimg \
+    libhwconverter \
+    libhwjpeg \
     libnetcmdiface \
     libsecion \
     libstlport \
